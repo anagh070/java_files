92bcd3e498f3756f3fb67d8960ae227db63d5d34
rudra <111167717+hiirudra@users.noreply.github.com>
2023-09-02 02:17:32 +0530

    Fixing add.java's logic and syntax
    Binary addition logic was not being performed correctly. Fixed it by typecasting the inputs.

--- add.java
+++ add.java
@@ -1,14 +1,30 @@
 import java.util.*;
-class add
+
+// This is a Java Program to add two Binary numbers which are entered by a user
+class Main
 {
     public static void main(String args[])
     {
-        int a,b,ans;
+        // Defining the variables to store the numbers as well as their sum
+        String a , b, ans;
+        int x , y;
+        // Importing scanner class
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter 1/0 for a , b");
-        a=sc.nextInt();
-        b=sc.nextInt();
-        ans=a+b;
+        
+        System.out.println("Enter a and b in binary.");
+        
+        // Taking binary user input from the user and storing as a string
+        a = sc.nextLine();
+        b = sc.nextLine();
+        
+        // Converting a and b to their respective decimal equivalent from user input binary stored as a string.
+        x = Integer.parseInt(a,2);
+        y = Integer.parseInt(b,2);
+        
+        // Performing addition of the decimal equivalent numbers.
+        ans = Integer.toBinaryString(x + y);
+        
+        // Printing the result
         System.out.println(ans);
     }
 }
